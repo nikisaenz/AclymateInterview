40d850467d8bcb71901959618c10faa255a4ac52
nikisaenz <62860138+nikisaenz@users.noreply.github.com>
2021-04-26 14:31:28 -0600

    schemaChecker
    final

--- schemaChecker.js
+++ schemaChecker.js
@@ -37,30 +37,3 @@ function schemaChecker(body, schema) {
 
 }
 
-const body = {
-    name: "William Loopesko",
-    age: 33,
-    founder: true,
-    company: "Aclymate",
-    address: {
-        description: "2432 S. Downing St, Denver, CO 80210",
-        county: "Denver",
-        country: "USA"
-    }
-}
-
-const schema = {
-    name: {type: "string", required: true},
-    age: {type: "number", required: true},
-    founder: {type: "boolean", required: false},
-    company: {type: "string", required: true},
-    address: {
-        type: "object",
-        required: true,
-        description: {type: "string", required: true},
-        county: {type: "string", required: false},
-        country: {type: "string", required: false}
-    }
-}
-
-schemaChecker(body, schema);
